b00221ecd69a911789a79963400d7df981d32ccd
Rikard Eide <rikardeide@gmail.com>
2023-03-22 01:28:36 +0100

    fish: add "confiure mode"

---
+++ .config/fish/functions/configure.fish
@@ -0,0 +1,5 @@
+function configure -w "configure dotfiles" -d "Rix: Alias git to work with dotfiles"
+  echo "🚧 Entering maintenance mode: git will be aliased to work with dotfiles"
+  alias git='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+end
+
