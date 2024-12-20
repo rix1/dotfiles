42ed585886ec6f2664529991e390f2f5ef6f18f6
Rikard Eide <rikardeide@gmail.com>
2024-12-20 19:49:19 +0100

    some otovo improvements

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -8,7 +8,7 @@ alias cat=bat
 
 # Region Cloud
 alias woc='z cloud && vf activate cloud'
-alias rss='gunicorn -w2 -t120 --reload cloud.wsgi'
+alias rss='gunicorn -w2 -t120 --reload --bind 0.0.0.0:8000 cloud.wsgi'
 alias rssp='ENVIRONMENT=production gunicorn -w2 -t120 --reload cloud.wsgi'
 # Endregion Cloud
 
@@ -16,4 +16,10 @@ alias pp=pnpm
 
 # Region kubernetes
 alias kb=kubectl
-alias cs='kubectl exec -it (kubectl get pods -o custom-columns=:metadata.name -n cloud | grep web | head -n1) -n cloud -- bash -c "python manage.py shell"'
+alias cs_old='kubectl exec -it (kubectl get pods -o custom-columns=:metadata.name -n cloud | grep web | head -n1) -n cloud -- bash -c "python manage.py shell"'
+alias cs='/Users/rix1/Development/otovo/cloud/scripts/k8s-shell.sh'
+alias wip='git add . && git commit -m "wip"'
+
+
+abbr --add djl djlint --reformat
+
