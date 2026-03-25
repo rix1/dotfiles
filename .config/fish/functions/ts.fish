76ea50c32f71b43099f13b5d800a6a0fd86b87bd
Rikard Eide <rikardeide@gmail.com>
2026-03-25 23:07:04 +0100

    update ts - tmux session switcher

--- .config/fish/functions/ts.fish
+++ .config/fish/functions/ts.fish
@@ -1,26 +1,23 @@
-function ts --description "Browse and select tmux sessions with fzf"
-    set -l selection (begin
-        echo "+ New session"
-        tmux list-sessions 2>/dev/null
-    end | fzf --header="Select tmux session" \
-              --preview 'test {} != "+ New session"; and tmux list-windows -t (echo {} | cut -d: -f1) 2>/dev/null; or echo "Create a new session"' \
-              --preview-window=right:50%:wrap)
+function ts --description "Browse and select tmux sessions with tv"
+    set -l sessions (tmux list-sessions 2>/dev/null)
 
-    or return 0
-
-    if test "$selection" = "+ New session"
-        read -P "Session name (leave empty for default): " session_name
+    if test -z "$sessions"
+        read -P "No sessions. Create one? Name (empty for default): " session_name
         if test -n "$session_name"
             tmux new-session -s $session_name
         else
             tmux new-session
         end
+        return
+    end
+
+    set -l session (tv tmux-sessions)
+
+    or return 0
+
+    if set -q TMUX
+        tmux switch-client -t $session
     else
-        set -l session (echo $selection | cut -d: -f1)
-        if set -q TMUX
-            tmux switch-client -t $session
-        else
-            tmux attach -t $session
-        end
+        tmux attach -t $session
     end
 end
